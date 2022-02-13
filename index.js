b40bf77a32801250a62707fd01b611697a51fa9d
ellenyche <76154253+ellenyche@users.noreply.github.com>
2022-02-14 03:45:15 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//
+//            $(".letter-img").on("mouseover",function(){
+//               $(".letter-img").effect( "shake", {times:3}, 1000 );
+//            });
+//
+// //         });
+//
+// $(document).ready(function() {
+//
+//            $("#button").click(function(){
+//               $(".target").effect( "shake", {times:3}, 1000 );
+//            });
+//
+//         });
